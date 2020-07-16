b9943386528a83f0302aca319b5d06ae607c8502
Johar Ali <64891225+Johar2503@users.noreply.github.com>
2020-07-16 11:27:18 +0530

    Add files via upload

---
+++ D2/A1.js
@@ -0,0 +1,49 @@
+console.log("Hello");
+
+let a=33;
+console.log(a);
+
+let st="Letsupgrade";
+console.log(st);
+
+let b=[1,"two",3,"hi"];
+console.log(b);
+
+let c;
+console.log(c);
+
+let d=null;
+console.log(d);
+
+let e=55.895465;
+console.log(e);
+
+let f="Hi this is assignment";
+console.log(f);
+
+console.log(parseInt(f));
+
+console.log(parseFloat(f));
+
+console.log(parseInt(e));
+
+let g=e.toFixed(3);
+console.log(g);
+
+console.log(typeof(a));
+
+console.log(typeof(e));
+console.log(typeof(c));
+
+console.log(typeof(b));
+
+console.log(typeof(st));
+
+//alert("Hello Lets upgrade");
+
+/*let rno=prompt("Enter your number:");
+console.log(rno);*/
+
+let j=confirm("Are u 18+?");
+console.log(j);
+
---
+++ D2/A2.js
@@ -0,0 +1,13 @@
+let text="How are you all.Hope all situation is under control.";
+console.log(text);
+
+console.log(text.substring(5,9));
+
+let ar=["one",2,"Letsupgrade","Helo"];
+console.log(ar);
+
+let a=ar.splice(2,1,"hi");
+console.log(a);
+
+console.log(text.replace("Hope","Nope"));
+
